fab73bb506032e0772af72d85cf1935264a68f81
nuritopbas7 <63564685+nuritopbas7@users.noreply.github.com>
2020-04-13 01:45:39 +0300

    bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.QvClI9G9xxHsNDXjhewk5ivYP8TLn4XD);//BOT_TOKEN is the Client Secret
